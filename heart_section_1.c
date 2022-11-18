3d882292308c7f014885d72576042763f1d6a7ca
Nineink <108967182+Truninel@users.noreply.github.com>
2022-11-18 17:40:38 +0800

    Update heart_section_1.c

--- heart_section_1.c
+++ heart_section_1.c
@@ -6,7 +6,6 @@ int main()
 {
 	system("color 2");
 	double x = 0.0, y = 0.0, f_y = 0.0;
-	//   一行       行数编码  函数值   
 	for (y = 2.0; y >= -2.0; y -= 0.05)
 	{
 		for (x = -2.0; x <= 2.0; x += 0.025)
